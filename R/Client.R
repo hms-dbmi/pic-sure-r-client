528d307ee455c212a2b65b9ee2c01462108a740f
Nick Benik <nbenik@gmail.com>
2019-05-10 17:19:55 -0400

    end of week commit

--- R/Client.R
+++ R/Client.R
@@ -1,8 +1,8 @@
-# PIC-SURE library
+# PIC-SURE Client Library
 
 
 #' export
-PicSureClient <- R6::R6Class("PicSureClient", 
+Client <- R6::R6Class("PicSureClient", 
                              portable = FALSE,
                              lock_objects = FALSE,                                 
                              public = list(
@@ -10,12 +10,10 @@ PicSureClient <- R6::R6Class("PicSureClient",
                                  print("PIC-SURE R Client Library (version 0.1.0)")
                                },
                                help = function() {
-                                 writeLines("\n")
-                                 writeLines("        [HELP] PicSureClient$new()")
-                                 writeLines("            .version()                      gives version information for library")
-                                 writeLines("            .connect(<url>, <token>)        returns a connection object")
-                                 writeLines("\n")
-                               },
+                                 writeLines("        [HELP] PicSureClient::Client$new()")
+                                 writeLines("            $version()                      gives version information for library")
+                                 writeLines("            $connect(<url>, <token>)        returns a connection object")
+                             },
                                connect = function(url, token) {
                                  return(PicSureConnection$new(url, token))
                                }
@@ -30,22 +28,26 @@ PicSureConnection <- R6::R6Class("PicSureConnection",
                                 public = list(
                                   initialize = function(url, token) {
                                     # TODO: trim and make sure URL ends in "/"
-                                    self$url <- url
+                                    endpoint <- str_trim(url)
+                                    if (str_detect(endpoint, "/$") == FALSE) {
+                                      endpoint <- paste(endpoint, "/", sep="")
+                                    }
+                                    self$url <- endpoint
                                     self$token <- token
                                   },
                                   help = function() {
-                                    writeLines("      [HELP] PicSureClient.Client.connect(url, token)")
-                                    writeLines("      .list()                         Prints a list of available resources")
-                                    writeLines("      .about(resource_uuid)           Prints details about a specific resource")
+                                    writeLines("      [HELP] PicSureClient$connect(url, token)")
+                                    writeLines("      $list()                         Prints a list of available resources")
+                                    writeLines("      $about(resource_uuid)           Prints details about a specific resource")
                                     writeLines("")
                                     writeLines("      [Connect to Resource]")
                                     writeLines("      To connect to a resource load its associated resource code library")
                                     writeLines("      and then pass the API connection object (this object) to the")
                                     writeLines("      the library's Client object like this:")
                                     writeLines("")
-                                    writeLines("            myPicSureClient <- PicSureClient$new()")
+                                    writeLines("            myPicSureClient <- PicSureClient::Client$new()")
                                     writeLines("            myPicSureConn <- myPicSureClient$connect(url, token)")
-                                    writeLines("            myResourceAdapter <- PicSureHpdsAdapter(myPicSureConn)")
+                                    writeLines("            myResourceAdapter <- PicSureHpdsLib::Adapter(myPicSureConn)")
                                     writeLines("            myResource <- myResourceAdapter$useResource(resource_uuid)")
                                     writeLines("            myResource$help()")
                                     writeLines("")
@@ -53,14 +55,30 @@ PicSureConnection <- R6::R6Class("PicSureConnection",
                                     writeLines("              a specific type which has its own adapter library.  Libraries will")
                                     writeLines("              follow the naming convention: \"PicSureXyzLib\" where \"Xyz\"")
                                     writeLines("              specifies the adapter's storage format.")
-                                    writeLines("")
                                   },
-                                  about = function(resourceId = NA) {
+                                  about = function(resourceId = FALSE) {
                                     # TODO: implement this
-                                    writeLines("Connection$about()\n")
+                                    urlstr = paste(self$url, "info", sep="")
+                                    if (resourceId != FALSE) {
+                                      urlstr = paste(urlstr, resourceId, sep="/")
+                                    } else {
+                                      urlstr = paste(urlstr, "resources", sep="/")
+                                    }
+                                    request = GET(urlstr, content_type_json(), accept_json(), add_headers(Authorization=paste('Bearer',self$token)))
+                                    if (request$status_code != 200) {
+                                      writeLines("ERROR: HTTP response was bad")
+                                      print(request)
+                                      return('{"results":{}, error":"True"}') 
+                                    } else {
+                                      return(content(request, "text"))
+                                    }
                                   },
                                   list = function() {
                                     writeLines("Connection$list()\n")
+                                    entries <- jsonlite::fromJSON(self$about(), auto_unbox=TRUE)
+                                    for (rec in entries) {
+                                      writeLines(c(rec$uuid, " -> ", rec$name), sep="")
+                                    }
                                   },
                                   getInfo = function() {
                                     writeLines("Connection$getInfo()\n")
@@ -85,9 +103,41 @@ PicSureConnectionAPI <- R6::R6Class("PicSureConnectionAPI",
                                         self$token <- token
                                       },
                                       info = function(resource_uuid) { writeLines(resource_uuid) },
-                                      search = function(resource_uuid, query) { writeLines(c(resource_uuid, query)) },
-                                      asynchQuery = function(resource_uuid, query) { writeLines(c(resource_uuid, query)) },
-                                      synchQuery = function(resource_uuid, query) { writeLines(c(resource_uuid, query)) },
+                                      search = function(resource_uuid, query) { 
+                                        full_url = paste(self$url, "search", "/", resource_uuid, sep="")
+                                        if (query == FALSE) {
+                                          query <- list()
+                                          query$query <- ""
+                                          query = jsonlite::toJSON(query, auto_unbox=TRUE)
+                                        }
+                                        request = POST(full_url, body=query, content_type_json(), accept_json(), add_headers(Authorization=paste('Bearer',self$token)))
+                                        if (request$status_code != 200) { 
+                                          writeLines("ERROR: HTTP response was bad")
+                                          print(request)
+                                          return('{"results":{}, error":"True"}') 
+                                        } else {
+                                          return(content(request, text))
+                                        }
+                                      },
+                                      asynchQuery = function(resource_uuid, query) { 
+                                        writeLines(c(resource_uuid, query))
+                                      },
+                                      synchQuery = function(resource_uuid, query) { 
+                                        full_url = paste(self$url, "query/sync/", resource_uuid, sep="")
+                                        if (query == FALSE) {
+                                          query <- list()
+                                          query$query <- ""
+                                          query = jsonlite::toJSON(query, auto_unbox=TRUE)
+                                        }
+                                        request = POST(full_url, body=query, content_type_json(), accept_json(), add_headers(Authorization=paste('Bearer',self$token)))
+                                        if (request$status_code != 200) { 
+                                          writeLines("ERROR: HTTP response was bad")
+                                          print(request)
+                                          return('{"results":{}, error":"True"}') 
+                                        } else {
+                                          return(content(request, text))
+                                        }
+                                      },
                                       queryStatus = function(resource_uuid, query_uuid) { writeLines(c(resource_uuid, query_uuid)) },
                                       queryResult = function(resource_uuid, query_uuid) { writeLines(c(resource_uuid, query_uuid)) }
                                     )
